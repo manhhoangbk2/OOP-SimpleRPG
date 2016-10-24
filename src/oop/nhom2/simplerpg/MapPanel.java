f56a3e1f8e708291e6f7b8135653714d3e03853f
Phan Ngoc Lan <phan.ngoclan58@gmail.com>
2016-10-24 15:42:28 +0700

    MapPanel update
    Load nền cho ô trong map bằng ảnh thay vì tô màu.
    Thêm tài nguyên ảnh

--- src/oop/nhom2/simplerpg/MapPanel.java
+++ src/oop/nhom2/simplerpg/MapPanel.java
@@ -2,8 +2,12 @@ package oop.nhom2.simplerpg;
 
 import java.util.Vector;
 import javax.swing.*;
+import javax.swing.border.LineBorder;
+
 import java.awt.Color;
 import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.Toolkit;
 
 @SuppressWarnings("serial")
 public class MapPanel extends JPanel {
@@ -11,34 +15,40 @@ public class MapPanel extends JPanel {
 	 * Terrain map:
 	 * 0 = unused/unreachable
 	 * 1 = grass
+	 * 1.0 = tree on grass
 	 * 2 = dirt
 	 * 3 = water*/
-	private JLabel [][] cells;
+	private BackgroundPanel [][] cells;
 	public MapPanel(String [][] layout, int rows, int cols, Vector<NPC> npc) {
-		cells = new JLabel[rows][cols];
+		this.setBorder(new LineBorder(Color.BLACK, 13));
+		cells = new BackgroundPanel[rows][cols];
 		setLayout(new GridLayout(rows, cols, 0, 0));
 		for(int i=0;i<rows;i++){
 			for(int j=0;j<cols;j++){
-				cells[i][j] = new JLabel();
-				Color cell_color;
+				Image cell_bg = null;
 				switch(layout[i][j]){
 					case "0":
-						cell_color = Color.BLACK;
+						cell_bg = Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("/images/no_tile.png"));
 						break;
 					case "1":
-						cell_color = new Color(51,255,51);
+						cell_bg = Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("/images/grass_tile.png"));
+						break;
+					case "1.0":
+						cell_bg = Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("/images/grass_tree_tile.png"));
 						break;
 					case "2":
-						cell_color = new Color(51,25,0);
+						cell_bg = Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("/images/dirt_tile.png"));
 						break;
 					case "3":
-						cell_color = new Color(24,28,135);
+						cell_bg = Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("/images/water_tile.png"));
 						break;
 					default:
-						cell_color = new Color(255,255,102);
+						cell_bg = Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("/images/transition_tile.png"));
+						break;
 				}
+				
+				cells[i][j] = new BackgroundPanel(cell_bg,BackgroundPanel.ACTUAL);
 				cells[i][j].setOpaque(true);
-				cells[i][j].setBackground(cell_color);
 				this.add(cells[i][j]);
 			}
 		}
